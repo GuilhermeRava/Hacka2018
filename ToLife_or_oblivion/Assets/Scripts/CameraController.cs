730069ae302354282f45f9aba348406786243155
Frederico Vicente <fm.vicente@campus.fct.unl.pt>
2018-03-17 15:36:02 +0000

    CameraController added

---
+++ ToLife_or_oblivion/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+
+    [SerializeField]
+    private Vector3 cameraPosition = new Vector3(3,6,5);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = target.transform.position + cameraPosition;
+        transform.LookAt(target.transform.position);
+
+	}
+}
