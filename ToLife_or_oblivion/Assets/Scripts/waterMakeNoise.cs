7efaa42f559ba21c7ea5de2c72dfbe2a74819a80
Frederico Vicente <fm.vicente@campus.fct.unl.pt>
2018-03-17 16:40:47 +0000

    Water script added

---
+++ ToLife_or_oblivion/Assets/Scripts/waterMakeNoise.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterMakeNoise : MonoBehaviour {
+
+    public float power;
+    public float scale;
+    public float timeScale;
+
+    private float xOffset;
+    private float yOffset;
+    private MeshFilter mf;
+
+	// Use this for initialization
+	void Start () {
+        mf.GetComponent<MeshFilter>();
+        makeNoise();
+	}
+
+	private void Update()
+	{
+        makeNoise();
+        xOffset = Time.deltaTime + timeScale;
+        yOffset = Time.deltaTime + timeScale;
+
+	}
+
+	void makeNoise() {
+        Vector3[] vertices = mf.mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i].y = calculateHeight(vertices[i].x, vertices[i].z) * power;
+        }
+    }
+
+    float calculateHeight(float x, float y) {
+
+        float coordX = x * scale + xOffset;
+        float coordY = y * scale + yOffset;
+
+        return Mathf.PerlinNoise(coordX,coordY);
+    }
+}
