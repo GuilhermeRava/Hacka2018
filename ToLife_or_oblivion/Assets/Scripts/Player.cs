1d0a2fe7d04554989f69782e143f5d4e23a6724b
Frederico Vicente <fm.vicente@campus.fct.unl.pt>
2018-03-17 15:28:56 +0000

    Player Movement Implementation

--- ToLife_or_oblivion/Assets/Scripts/Player.cs
+++ ToLife_or_oblivion/Assets/Scripts/Player.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    CharacterController _controller;
+
+    [SerializeField]
+    private float speed;
+
+    private const float gravity = 9.8f;
+
 	// Use this for initialization
 	void Start () {
+
+        _controller = GetComponent<CharacterController>();
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        movePlayer();
 	}
+
+    private void movePlayer() {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput,0,verticalInput).normalized;
+        Vector3 velocity = direction * speed;
+
+        velocity.y -= gravity;
+
+        velocity = transform.transform.TransformDirection(velocity);
+
+        _controller.Move(velocity * Time.deltaTime);
+
+    }
+
 }
